0d2dbb9243923709008c3a63d4dd68c0ae6bba76
carlos larrondo <carlos.larrondo@mariaanasanz.es>
2020-10-05 14:45:42 +0200

    prueba

--- TestEstudiante.java
+++ TestEstudiante.java
@@ -16,6 +16,8 @@ public class TestEstudiante
         e1.printEstudiante();
         Estudiante e2 = new Estudiante("Ana", 8);
         e2.printEstudiante();
+        Estudiante e3 = new Estudiante("Anita", 80);
+        e3.printEstudiante();
 
     }
 }
